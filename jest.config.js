e1f0cc60a105e8d5a7996463355d3b1b1e527a05
Predjo <predovan.nikola@gmail.com>
2020-02-17 12:04:40 +0100

    remove test regex

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
-  // testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   testEnvironment: "jsdom",
   moduleFileExtensions: [
     "ts",
